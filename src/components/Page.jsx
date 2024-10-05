4e09c4fb9b8a33acfa4eaf00e2d0812711200601
AbdullahEskeif <abdullah.m.eskef@gmail.com>
2024-10-05 18:57:18 +0300

    test

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,6 +1,6 @@
 const Page = () => {
   return (
-    <nav className="flex justify-center bg-red-600 text-white">Hello World</nav>
+    <nav className="flex justify-center bg-red-600 text-white"> Testing  Neogit Hello World</nav>
   );
 };
 
